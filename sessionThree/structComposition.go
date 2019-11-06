1b8fdd679b4ab2a82caebffb000c7a478092543d
Mayank Gupta (IBAM) <Mayank.Gupta@iontrading.com>
2019-11-06 19:21:25 +0530

    Object Composition

--- sessionThree/structComposition.go
+++ sessionThree/structComposition.go
@@ -7,3 +7,28 @@
 5. Overriding Composed Level Function in the Parent Function
 
 */
+
+package main
+
+import "fmt"
+
+type Employee struct {
+	Name        string
+	Age         int
+	Designation string
+}
+
+func (empo Employee) getDetails() {
+	fmt.Println(empo.Designation + " " + empo.Name)
+}
+
+type Manager struct {
+	Employee
+	TeamSize int
+}
+
+func main() {
+	newManager := Manager{Employee{"Mayank", 12, "Developer"}, 19}
+	newManager.Name = "Mayank"
+	newManager.getDetails()
+}
