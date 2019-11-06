12afeccf0197e1f943d42249c45fda9490b78043
Mayank Gupta (IBAM) <Mayank.Gupta@iontrading.com>
2019-11-06 22:34:23 +0530

    Changes

---
+++ sessionFour/workingWithSlices.go
@@ -0,0 +1,34 @@
+/*
+
+1. Understanding Difference between length and capacity
+2. Slices are memory locations
+3. Slices are passed by reference
+4. Slice can be a sub part of an array
+5. Output for "fmt.Println" for slice is not starting memory reference of the Slice element
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// Undersstanding Capacity and Length Difference...
+	sliceData := []int{1, 2, 3}
+	fmt.Println(cap(sliceData))
+	fmt.Println(len(sliceData))
+
+	sliceData = append(sliceData, 4)
+	fmt.Println(cap(sliceData))
+	fmt.Println(len(sliceData))
+
+	sliceData = append(sliceData, 5)
+	sliceData = append(sliceData, 6)
+	fmt.Println(len(sliceData))
+
+	sliceData = append(sliceData, 7)
+	sliceData = append(sliceData, 8)
+	fmt.Println(cap(sliceData))
+	fmt.Println(len(sliceData))
+}
