5d916aef7a68567f8319d85cc9a318a3b6369a39
Mayank Gupta (IBAM) <Mayank.Gupta@iontrading.com>
2019-11-07 21:15:37 +0530

    Changes

--- sessionSix/creatingError.go
+++ sessionSix/creatingError.go
@@ -2,5 +2,26 @@
 
 1. Creating Custom Functions returning Errors using "errors.New"
 2. Error Last Approach in Golang
+3. Value passed to "error.New" is received as error value...
 
-*/ 
+*/
+
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	returnData, err := ThrowErrorFunc()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(returnData)
+	}
+}
+
+func ThrowErrorFunc() (int, error) {
+	return 0, errors.New("Invalid Operation Data...")
+}
