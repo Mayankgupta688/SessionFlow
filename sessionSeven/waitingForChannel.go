0c1ccb6bacb5b2105e857735e03e706627541e5d
Mayank Gupta (IBAM) <Mayank.Gupta@iontrading.com>
2019-11-08 11:02:16 +0530

    Changes

--- sessionSeven/waitingForChannel.go
+++ sessionSeven/waitingForChannel.go
@@ -1,9 +1,39 @@
+/*
+
+1. Create a Main Function
+2. Add a Goroutine
+3. Pass channel
+4. Add Data in Channel in Gorouting
+5. Consume data from the channel
+6. Create ConsumerFunction and ProducerFunction for Channel..
+
+*/
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Main Execution Block Starts")
 
+	ch := make(chan int)
+
+	go RoutingProducer(ch)
+
+	go RoutingConsumer(ch)
+
+	time.Sleep(5 * time.Second)
+
 	fmt.Println("Main Execution Block Stops")
+}
+
+func RoutingProducer(ch chan int) {
+	ch <- 10
+}
+
+func RoutingConsumer(ch chan int) {
+	fmt.Println(<-ch)
 }
